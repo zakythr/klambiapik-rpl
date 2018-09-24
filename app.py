f7dc9edd6425ed3867500d889fa0702e999d3816
zakythr <zakythariq@gmail.com>
2018-09-24 19:58:42 +0700

    ingat1

--- app.py
+++ app.py
@@ -55,19 +55,19 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    text = event.message.text #simplify for receove message
-    sender = event.source.user_id #get usesenderr_id
-    gid = event.source.sender_id #get group_id
-    profile = line_bot_api.get_profile(sender)
-    if text=="adit":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat adit'))
-    if text=="mail":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat mail'))
-    if text=="djohan":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat djohan'))
-    if text=="Bella":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Rumah kamu dimana?'))
+# def handle_message(event):
+#     text = event.message.text #simplify for receove message
+#     sender = event.source.user_id #get usesenderr_id
+#     gid = event.source.sender_id #get group_id
+#     profile = line_bot_api.get_profile(sender)
+#     if text=="adit":
+#         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat adit'))
+#     if text=="mail":
+#         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat mail'))
+#     if text=="djohan":
+#         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat djohan'))
+#     if text=="Bella":
+#         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Rumah kamu dimana?'))
 
 def handle_message(event):
     text = event.message.text #simplify for receove message
