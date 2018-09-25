0b1bc8dea39be2f3784a0e5246da6a94ba4dcd1d
zakythr <zakythariq@gmail.com>
2018-09-25 16:44:18 +0700

    issues2

--- app.py
+++ app.py
@@ -64,7 +64,19 @@ def handle_message(event):
     if text=="mail":
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat mail'))
     if text=="djohan":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu baik djohan'))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat djohan'))
+    if text=="Bella":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Rumah kamu dimana?'))
+
+    text = event.message.text #simplify for receove message
+    sender = event.source.user_id #get usesenderr_id
+    gid = event.source.sender_id #get group_id
+    profile = line_bot_api.get_profile(sender)
+    a=(randint(0, 9))
+    if a%2:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Iya'))
+    else:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Tidak'))
 
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
 
