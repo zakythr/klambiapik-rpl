ad9855cdaba717625410fc55f900b0d244260583
zakythr <zakythariq@gmail.com>
2018-10-01 07:34:09 +0700

    'ge'

--- app.py
+++ app.py
@@ -1,103 +1,96 @@
-from __future__ import unicode_literals, absolute_import
+from __future__ import unicode_literals
 
-import json
-import unittest
+import errno
+import os
+import sys
+import tempfile
+import requests
+import re
 
-import responses
+from argparse import ArgumentParser
+
+from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    LineBotApiError, InvalidSignatureError
 )
 from linebot.models import (
-    AudioSendMessage
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    PostbackAction, DatetimePickerAction,
+    CameraAction, CameraRollAction, LocationAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    TextComponent, SpacerComponent, IconComponent, ButtonComponent,
+    SeparatorComponent, QuickReply, QuickReplyButton
 )
 
+app = Flask(__name__)
 
-class TestLineBotApi(unittest.TestCase):
-    def setUp(self):
-        self.tested = LineBotApi('channel_secret')
+line_bot_api = LineBotApi('In++hTLWDsGi8712BFxRaC9Qkr/Lbn8fQe6dhkBm+8Fxl6zH19I97WzYRsKEJlX9ZgCJb9DAWSTuEFedqenfMHdQuDraavqIjrGVNibdCS8idf2QmCNpmQqqd9flJQyxNgX4tjBdncjkyTYKA3N4ogdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('1aa040aad1ecfbcf33d3a5916b4a1439')
 
-        self.audio_message = AudioSendMessage(
-            original_content_url='http://laguaz.net/listen-song/pasar-bisa-diciptakan-vQ9JGckNpS',
-            duration=240000
-        )
+notes == {}
 
-        self.message = [{
-            "type": "audio",
-            "originalContentUrl": "http://laguaz.net/listen-song/pasar-bisa-diciptakan-vQ9JGckNpS",
-            "duration": 240000,
-        }]
-
-    @responses.activate
-    def test_push_audio_message(self):
-        responses.add(
-            responses.POST,
-            LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/push',
-            json={}, status=200
-        )
-
-        self.tested.push_message('to', self.audio_message)
-
-        request = responses.calls[0].request
-        self.assertEqual(request.method, 'POST')
-        self.assertEqual(
-            request.url,
-            LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/push'
-        )
-        self.assertEqual(
-            json.loads(request.body),
-            {
-                "to": "to",
-                "messages": self.message
-            }
-        )
-
-    @responses.activate
-    def test_reply_audio_message(self):
-        responses.add(
-            responses.POST,
-            LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/reply',
-            json={}, status=200
-        )
-
-        self.tested.reply_message('replyToken', self.audio_message)
-
-        request = responses.calls[0].request
-        self.assertEqual(request.method, 'POST')
-        self.assertEqual(
-            request.url,
-            LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/reply')
-        self.assertEqual(
-            json.loads(request.body),
-            {
-                "replyToken": "replyToken",
-                "messages": self.message
-            }
-        )
-
-    @responses.activate
-    def test_multicast_audio_message(self):
-        responses.add(
-            responses.POST,
-            LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/multicast',
-            json={}, status=200
-        )
-
-        self.tested.multicast(['to1', 'to2'], self.audio_message)
-
-        request = responses.calls[0].request
-        self.assertEqual(request.method, 'POST')
-        self.assertEqual(
-            request.url,
-            LineBotApi.DEFAULT_API_ENDPOINT + '/v2/bot/message/multicast')
-        self.assertEqual(
-            json.loads(request.body),
-            {
-                "to": ['to1', 'to2'],
-                "messages": self.message
-            }
-        )
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
 
-if __name__ == '__main__':
-    unittest.main()
+# function for create tmp dir for download content
+def make_static_tmp_dir():
+    try:
+        os.makedirs(static_tmp_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
+            pass
+        else:
+            raise
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except LineBotApiError as e:
+        print("Got exception from LINE Messaging API: %s\n" % e.message)
+        for m in e.error.details:
+            print("  %s: %s" % (m.property, m.message))
+        print("\n")
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    text = event.message.text #simplify for receove message
+    sender = event.source.user_id #get usesenderr_id
+    gid = event.source.sender_id #get group_id
+    profile = line_bot_api.get_profile(sender)
+    if text=="adit":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat adit'))
+    if text=="mail":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat mail'))
+    if text=="djohan":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat djohan'))
+
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
