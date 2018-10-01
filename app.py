123e03101e7de8d057f331834d7f50418e9ee63f
zakythr <zakythariq@gmail.com>
2018-10-01 08:19:15 +0700

    'harus'

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def handle_location_message(event):
 
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
-    line_bot_api.reply_message(event.reply_token, StickerSendMessage(package_id=event.message.package_id, sticker_id=event.message.sticker_id))
+    line_bot_api.reply_message(event.reply_token, StickerSendMessage(package_id=1, sticker_id=1))
 
 import os
 if __name__ == "__main__":
