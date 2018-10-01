117b91cd4c2e0c696c1741180b426d3ba64b2cf4
zakythr <zakythariq@gmail.com>
2018-10-01 13:27:54 +0700

    'ter'

--- app.py
+++ app.py
@@ -66,11 +66,11 @@ def handle_message(event):
     gid = event.source.sender_id #get group_id
     profile = line_bot_api.get_profile(sender)
     if text=="adit":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat adit'),StickerSendMessage(package_id='1', sticker_id='1'))
+        line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='https://cdn2.boombastis.com/wp-content/uploads/2017/09/lukisanp.jpg',preview_image_url='https://cdn2.boombastis.com/wp-content/uploads/2017/09/lukisanp.jpg'))
     if text=="mail":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat mail'),StickerSendMessage(package_id='1', sticker_id='1'))
+        line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='https://cdn2.boombastis.com/wp-content/uploads/2017/09/lukisanp.jpg',preview_image_url='https://cdn2.boombastis.com/wp-content/uploads/2017/09/lukisanp.jpg'))
     if text=="djohan":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat djohan'),StickerSendMessage(package_id='1', sticker_id='1'))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat djohan'))
 
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
 
