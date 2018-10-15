7422d1bca0d960364d59d38700d66a8cb606473c
zakythr <zakythariq@gmail.com>
2018-10-15 15:59:38 +0700

    ub

--- app.py
+++ app.py
@@ -69,39 +69,6 @@ def handle_message(event):
         pro = "Wong suroboyo terkenal karo kesangarane. Sak piro sangarmu cak?\n1. lihat-[id]\n2. tambah-[id]-[kesangaran]\n3. hapus-[id]\n4. ganti-[id lama]-[id baru]-[kesangaran baru]\n5. kabeh"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=pro))
 
-#SUB MENU SANGAR
-    if(data[0]=='lihat'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=carimhs(data[1])))
-    elif(data[0]=='tambah'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=inputmhs(data[1],data[2])))
-    elif(data[0]=='hapus'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=hapusmhs(data[1])))
-    elif(data[0]=='ganti'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=updatemhs(data[1],data[2],data[3],data[4])))
-    elif(data[0]=='kabeh'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=allsmhs()))
-
-    elif(data[0]=='kamus'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=inputput(data[1],data[2])))
-
-#SPAM
-    elif (data[0]=='/spam'):
-        i = 0
-        if(int(data[2])>25):
-            if isinstance(event.source, SourceGroup):
-                line_bot_api.push_message(event.source.group_id,TextSendMessage(text="kakean woi, nggarakno server lemot ae"))
-            elif isinstance(event.source, SourceRoom):
-                line_bot_api.push_message(event.source.room_id,TextSendMessage(text="kakean woi, nggarakno server lemot ae"))
-        else:
-            while i < int(data[2]):
-                if isinstance(event.source, SourceGroup):
-                    line_bot_api.push_message(event.source.group_id,TextSendMessage(text=data[1]))
-                elif isinstance(event.source, SourceRoom):
-                    line_bot_api.push_message(event.source.room_id,TextSendMessage(text=data[1]))
-                #else:
-                #   line_bot_api.push_message(event.source.user_id,TextSendMessage(text=data[1]))
-                i =i+1
-
 #TINGGALKAN GROUP/ROOM
     elif text=="/bye":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Pingin ngekick aku?:(\nketik "/start" gawe ngekick!'))
